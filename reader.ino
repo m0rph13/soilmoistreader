66a4e4b3c59205b05c702efc89b5852c0648cc66
Magnus Rydin <magnus.rydin@istone.se>
2016-05-20 16:25:30 +0200

    first version

---
+++ reader.ino
@@ -0,0 +1,27 @@
+int moistSensorPins[] = {A1};
+int delayInMinis=5000;
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  for (int moistSensorPin = 0; moistSensorPin < (sizeof(moistSensorPins)/sizeof(int)); moistSensorPin++) 
+  {
+    Serial.print(moistSensorPin);
+    Serial.print(" = ");
+    Serial.print(readSensor(moistSensorPins[moistSensorPin]));
+    Serial.println(" %");
+  }
+  delay(delayInMinis);
+}
+
+double readSensor(int moistSensorPin)
+{
+  int sensorValue = analogRead(moistSensorPin);
+  double percentage=100*(1.0-(sensorValue/1024.0));
+  return percentage;
+}
+
